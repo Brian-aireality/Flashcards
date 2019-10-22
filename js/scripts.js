36949701f62928e690a5665b34c09a487f37f14a
Student <example@example.com>
2019-10-22 16:10:20 -0700

    reduce js script to 5 lines, add fade,toggle to discription

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,5 @@
 $(document).ready(function() {
-  $("#card00").click(function() {
-    $("#card00 .definition").fadeToggle();
-  })
-  $("#card10").click(function() {
-    $("#card10 .definition").fadeToggle();
-  })
-  $("#card20").click(function() {
-    $("#card20 .definition").fadeToggle();
-  })
-  $("#card01").click(function() {
-    $("#card01 .definition").fadeToggle();
-  })
-  $("#card11").click(function() {
-    $("#card11 .definition").fadeToggle();
-  })
-  $("#card21").click(function() {
-    $("#card21 .definition").fadeToggle();
-  })
-  $("#card02").click(function() {
-    $("#card02 .definition").fadeToggle();
-  })
-  $("#card12").click(function() {
-    $("#card12 .definition").fadeToggle();
-  })
-  $("#card22").click(function() {
-    $("#card22 .definition").fadeToggle();
-  })
-})
+  $(".card").click(function() {
+    $(this).find(".definition").fadeToggle();
+  });
+});
